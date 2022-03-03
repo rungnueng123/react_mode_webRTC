54c902c1541e6a4fb595b6e5c83e21f8f2eff7d0
rungnueng <rungnueng19@gmail.com>
2022-03-03 17:41:25 +0700

    remove api folder

--- api/routes/testApi.js
+++
@@ -1,8 +0,0 @@
-let express = require( 'express' );
-let router = express.Router();
-
-router.get("/check/api", function(req, res, next){
-    res.send("API is working")
-});
-
-module.exports = router
